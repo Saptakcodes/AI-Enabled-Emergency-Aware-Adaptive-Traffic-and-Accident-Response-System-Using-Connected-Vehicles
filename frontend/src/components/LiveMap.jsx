02eb408b21952ba956f27c5c0b64590d6d9dd8fa
Saptak <i.am.saptakchaki@gmail.com>
2026-03-11 21:38:17 +0530

    debugging

--- frontend/src/components/LiveMap.jsx
+++ frontend/src/components/LiveMap.jsx
@@ -1,104 +1,72 @@
 // src/components/LiveMap.jsx
-import React, { useState, useCallback } from 'react';
-import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-console.log("🔑 API Key present:", !!import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
+// Fix default marker icons in Leaflet with Webpack
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
 
-const containerStyle = {
-  width: '100%',
-  height: '100%',
-  borderRadius: '0.5rem'
-};
+// Custom icons (optional)
+const vehicleIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
-const defaultCenter = {
-  lat: 22.5726,
-  lng: 88.3639
-};
+const accidentIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
-const iconMap = {
-  ambulance: '/ambulance-icon.png',
-  police: '/police-icon.png',
-  fire: '/fire-icon.png',
-  normal: '/car-icon.png',
-  accident: '/accident-icon.png'
-};
-
-const LiveMap = ({ vehicles = [], accidents = [], center = defaultCenter, zoom = 14 }) => {
-  const [selectedVehicle, setSelectedVehicle] = useState(null);
-  const [selectedAccident, setSelectedAccident] = useState(null);
-
-  const onLoad = useCallback((map) => {
-    // Optional map setup
-  }, []);
-
+const LiveMap = ({ vehicles = [], accidents = [], center = [22.5726, 88.3639], zoom = 14 }) => {
   return (
-    <LoadScript
-      googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-      onError={(error) => console.error("❌ Google Maps load error:", error)}
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={zoom}
-        onLoad={onLoad}
-      >
-        {/* Vehicle Markers */}
-        {vehicles.map((vehicle) => (
-          <Marker
-            key={vehicle.blackbox_id}
-            position={{ lat: vehicle.latitude, lng: vehicle.longitude }}
-            icon={{
-              url: iconMap[vehicle.vehicle_type] || iconMap.normal,
-              scaledSize: { width: 30, height: 30 }   // ✅ fixed: plain object instead of google.maps.Size
-            }}
-            onClick={() => setSelectedVehicle(vehicle)}
-          />
-        ))}
-
-        {/* Accident Markers */}
-        {accidents.map((accident) => (
-          <Marker
-            key={accident._id}
-            position={{ lat: accident.latitude, lng: accident.longitude }}
-            icon={{
-              url: iconMap.accident,
-              scaledSize: { width: 40, height: 40 }   // ✅ fixed
-            }}
-            onClick={() => setSelectedAccident(accident)}
-          />
-        ))}
-
-        {/* InfoWindow for selected vehicle */}
-        {selectedVehicle && (
-          <InfoWindow
-            position={{ lat: selectedVehicle.latitude, lng: selectedVehicle.longitude }}
-            onCloseClick={() => setSelectedVehicle(null)}
-          >
-            <div className="p-2 text-black">
-              <h3 className="font-semibold">Vehicle {selectedVehicle.blackbox_id}</h3>
-              <p>Speed: {selectedVehicle.speed_kmph} km/h</p>
-              <p>Acceleration: {selectedVehicle.acceleration_g} g</p>
-              <p>Fire: {selectedVehicle.fire_detected ? '🔥' : 'No'}</p>
-            </div>
-          </InfoWindow>
-        )}
-
-        {/* InfoWindow for selected accident */}
-        {selectedAccident && (
-          <InfoWindow
-            position={{ lat: selectedAccident.latitude, lng: selectedAccident.longitude }}
-            onCloseClick={() => setSelectedAccident(null)}
-          >
-            <div className="p-2 text-black">
-              <h3 className="font-semibold text-red-600">Accident</h3>
-              <p>Speed: {selectedAccident.speed_kmph} km/h</p>
-              <p>Tilt: {selectedAccident.tilt_degree}°</p>
-              <p>Fire: {selectedAccident.fire_detected ? 'Yes' : 'No'}</p>
-            </div>
-          </InfoWindow>
-        )}
-      </GoogleMap>
-    </LoadScript>
+    <MapContainer center={center} zoom={zoom} style={{ height: '100%', width: '100%', borderRadius: '0.5rem' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {vehicles.map(vehicle => (
+        <Marker
+          key={vehicle.blackbox_id}
+          position={[vehicle.latitude, vehicle.longitude]}
+          icon={vehicleIcon}
+        >
+          <Popup>
+            <b>Vehicle {vehicle.blackbox_id}</b><br/>
+            Speed: {vehicle.speed_kmph} km/h<br/>
+            Acceleration: {vehicle.acceleration_g} g<br/>
+            Fire: {vehicle.fire_detected ? '🔥' : 'No'}
+          </Popup>
+        </Marker>
+      ))}
+      {accidents.map(accident => (
+        <Marker
+          key={accident._id}
+          position={[accident.latitude, accident.longitude]}
+          icon={accidentIcon}
+        >
+          <Popup>
+            <b style={{color: 'red'}}>Accident</b><br/>
+            Speed: {accident.speed_kmph} km/h<br/>
+            Tilt: {accident.tilt_degree}°<br/>
+            Fire: {accident.fire_detected ? 'Yes' : 'No'}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 };
 
